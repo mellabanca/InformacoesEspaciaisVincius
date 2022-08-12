3f77e4114727de2b10e230c17238a53c8dd4e120
mellabanca <mellabanca@gmail.com>
2022-08-11 22:06:42 -0300

    Versao1

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from './screens/Home';
+import IssLocationScreen from './screens/IssLocation';
+import MeteorsScreen from './screens/Meteors';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home"
+                       screenOptions={{headerShown: false}}>
+        <Stack.Screen name = "Home" component={HomeScreen}/>
+        <Stack.Screen name = "IssLocation" component={IssLocationScreen}/>
+        <Stack.Screen name = "Meteors" component={MeteorsScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
