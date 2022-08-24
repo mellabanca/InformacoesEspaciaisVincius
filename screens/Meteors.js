291cb944cd45b37a3fcdb8769d31f4a685996f0d
mellabanca <mellabanca@gmail.com>
2022-08-23 21:44:40 -0300

    Versão 4

--- screens/Meteors.js
+++ screens/Meteors.js
@@ -1,8 +1,56 @@
+import axios from "axios";
 import React, {Component} from "react";
-import {Text, View} from "react-native";
+import {Alert, Text, View, StyleSheet} from "react-native";
 
 export default class MeteorsScreen extends Component {
-    render(){
+    constructor(props){
+        super(props);
+        this.state = {
+            meteors: {}
+        }
+    }
+
+    componentDidMount(){
+        this.getMeteors();
+    }
+
+
+    getMeteors = () => {
+        axios
+        .get("https://api.nasa.gov/neo/rest/v1/feed?api_key=oPDaMFkUOTWW5hvSXQvEs4lPzwzZPRQHB2sDUOAG")
+        .then(response => {
+            this.setState({meteors: response.data.near_earth_objects})
+        })
+        .catch(error => {
+            Alert.alert(error.message);
+        })
+    }
+    
+    render(){;
+        if(Object.keys(this.state.meteors).length === 0){
+            return(
+                <View style = {styles.loading}>
+                    <Text>Carregando...</Text>
+                </View>
+            )
+        } else {
+            let matrizMeteor = Object.keys(this.state.meteors).map(dadosMeteors => {
+                return this.state.meteors[dadosMeteors]
+            })
+            let meteors = [].concat.apply([],matrizMeteor)
+
+            meteors.forEach(function(element){
+                let diametroMedio = 
+                (element.estimated_diameter.kilometers.estimated_diameter_min + 
+                 element.estimated_diameter.kilometers.estimated_diameter_max) / 2;
+
+                let pontAmeaca =
+                (diametroMedio / element.close_approach_data[0].miss_distance.kilometers)
+                * 1000000000;
+
+                element.ameacaPont = pontAmeaca;
+            })
+
         return(
             <View style = {{
                 flex: 1,
@@ -14,3 +62,12 @@ export default class MeteorsScreen extends Component {
         )
     }
 }
+}
+
+const styles = StyleSheet.create({
+   loading: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+   } 
+})
